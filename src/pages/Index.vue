9de8ce4e43ade679f50dbc5ab9de5e31bfbe1178
Franco Estrella <francoestrella55@gmail.com>
2021-02-22 19:16:30 -0300

    second commit of course quasar

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,31 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
+  <q-page padding>
+    <input type="text" 
+      v-model="message" 
+      @keyup.esc="ClearMessage" 
+      @keyup.enter="AlertMessage"
+    />
+    <button @click="ClearMessage">Clear</button>
+    <h5>{{message}}</h5>
   </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex'
+  data(){
+    return {
+      message: 'I love VueJs so Hard!'
+    }
+  },
+  methods: {
+    ClearMessage() {
+      this.message = '';
+    },
+    AlertMessage(){
+      alert(this.message);
+    }
+  }
 }
 </script>
+<style>
+</style>
