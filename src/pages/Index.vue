da3dc1461136ae8715e8bc33765116f81bc3d8dd
Franco Estrella <francoestrella55@gmail.com>
2021-03-30 01:59:01 -0300

    se sigue con el curso de quasar.

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,12 +1,29 @@
 <template>
   <q-page padding>
+    <button
+    style="position: absolute; right:10px;"
+    @click="counter++"
+
+    >
+      {{counter}}
+    </button>
     <input type="text" 
       v-model="message" 
       @keyup.esc="ClearMessage" 
       @keyup.enter="AlertMessage"
     />
+
     <button @click="ClearMessage">Clear</button>
-    <h5>{{message}}</h5>
+
+    <h5 
+    class="border-grey" 
+    v-if="message.length">{{message}}
+    </h5>
+    <h6 v-else>No message entered 🤔</h6>
+
+    <hr>
+
+    <p>messaje to UPPERCASE <b v-if="message.length">{{messadeUppercase}}</b><b v-else>No hay mensaje para mostrar</b></p>
   </q-page>
 </template>
 
@@ -14,8 +31,14 @@
 export default {
   data(){
     return {
-      message: 'I love VueJs so Hard!'
+      message: 'I love VueJs so Hard!',
+      counter: 0
     }
+  },
+  computed:{
+    // los metodos computados no son funciones tecnicamente, pero se escriben igual.
+    messadeUppercase(){ return this.message.toUpperCase() }
+    
   },
   methods: {
     ClearMessage() {
@@ -24,8 +47,12 @@ export default {
     AlertMessage(){
       alert(this.message);
     }
+    
   }
 }
 </script>
 <style>
+.border-grey{
+  border: 1px solid grey;
+}
 </style>
